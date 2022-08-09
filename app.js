d9008699bc3f7aaf988d75e0b0d58c2671d104af
Karim Isaias <karim-jahzeel.isaias@atos.net>
2022-08-09 16:32:06 -0500

    Toggle Buttons

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-$(document).bind('contextmenu', () => {
-    return false;
+$(document).ready(() => {
+    $('#toggle').on('click', () => {
+        if($('#submit').is(':disabled')){
+            $('#submit').removeAttr('disabled');
+        } else {
+            $('#submit').attr('disabled', 'disabled');
+        }
+    }); 
 });
