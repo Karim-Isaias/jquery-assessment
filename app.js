7e8d2262d862903a39493181616686ad9d5a466c
Karim Isaias <karim-jahzeel.isaias@atos.net>
2022-08-10 16:37:07 -0500

    JQuery 13

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 $(document).ready(() => {
+    colors = { "color1": "Red", "color2": "Green", 'color3': "Blue" };
 
+    $.each(colors, function(key, value) {
+        $('#divSelect').append($("<option/>", {
+            value: key,
+            text: value
+        }));
+    });
 });
-
-const moveButton = (elem) => {
-    if( $(elem).parent().attr("id") == "nonSelected" ){
-        $(elem).detach().appendTo('#selected');
-    }
-    else{
-        $(elem).detach().appendTo('#nonSelected'); 
-    }
-}
