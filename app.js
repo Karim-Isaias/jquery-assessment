2138abfb9a1e56aa602aed5286508d8ef6509eec
Karim Isaias <karim-jahzeel.isaias@atos.net>
2022-08-10 16:46:59 -0500

    JQuery 14

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 $(document).ready(() => {
-    colors = { "color1": "Red", "color2": "Green", 'color3': "Blue" };
-
-    $.each(colors, function(key, value) {
-        $('#divSelect').append($("<option/>", {
-            value: key,
-            text: value
-        }));
-    });
+    $('#newListElement').on('click', () => {
+        $("#header ul").append('<li>Java</li>');
+    })
 });
+
+// const new_element = () => {
+//     $("#header ul").append('<li>Java</li>');
+// }
