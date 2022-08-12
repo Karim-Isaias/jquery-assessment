e76d5c3b70b96121e36061257adabe5724f937d1
Karim Isaias <karim-jahzeel.isaias@atos.net>
2022-08-12 16:10:51 -0500

    JQuery 17

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 $(document).ready(() => {
-    $('p').each(function () {
-
-        var text_words = $(this).text().split(' ');
-
-        $(this).empty().html(function () {
-
-            for (i = 0; i < text_words.length; i++) {
-                if (i === 0) {
-                    $(this).append('<span>' + text_words[i] + '</span>');
-                } else {
-                    $(this).append(' <span>' + text_words[i] + '</span>');
-                }
-            }
-
-        });
-
-    });
+    $('input').on('keyup', () => {
+        let tvalue = $('input').val();
+        console.log(tvalue);
+    })
 });
